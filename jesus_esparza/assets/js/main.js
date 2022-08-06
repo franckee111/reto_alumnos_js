68fb1b9f13dfb93161ab4062aa239e03e273b74f
franckee111 <frestrada.dwfsc9@ucamp.utel.edu.mx>
2022-08-06 10:53:59 -0500

    corregir bug de tipo de dato

--- jesus_esparza/assets/js/main.js
+++ jesus_esparza/assets/js/main.js
@@ -5,16 +5,16 @@ var apellido = prompt("Ingrese sus apellidos");
 
 alert("Su nombre es: " + nombre + " " + apellido);
 
-var number,a = prompt("Ingrese el primer valor: ");
-var number,b = prompt("Ingrese el segundo valor: ");
+var a = prompt("Ingrese el primer valor: ");
+var b = prompt("Ingrese el segundo valor: ");
 var password = "AgrRkeTz";
 
 function funcionSuma(numero1, numero2) {
-    return (numero1 + numero2);
+    return Number(numero1) + Number(numero2);
 }
 
 function funcionResta(numero1, numero2) {
-    return numero1 - numero2;
+    return Number(numero1) - Number(numero2);
 }
 
 alert("La suma es " + funcionSuma(a, b));
